a2f43d1481f88eb3145b599801b12151499344c3
Madhav Paudel <poudelmadhav143@gmail.com>
2020-03-05 10:57:57 +0545

    Product scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/:locale/fun', to: 'fun#index'
   root 'fun#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_05_051219) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
